e19c3035037e27f75aef515d182b3171dc9078c1
FlameOnOn <31836982+FlameOnOn@users.noreply.github.com>
2018-12-24 17:42:01 +0800

    Create xgboostWine.py

---
+++ xgboost/xgboostWine.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 24 16:45:23 2018
+
+@author: sijinc
+"""
+
+import xgboost as xgb
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+
+def show_accuracy(a, b):
+    acc = a.ravel() == b.ravel()
+    print(float(acc.sum())/a.size)
+    
+
+if __name__ == "__main__":
+    path = 'D:\\mldata\\XGBoost\\wine.data'
+    data = np.loadtxt(path, dtype=float, delimiter=',')
+    y,x = np.split(data, (1,), axis=1)
+    x = StandardScaler().fit_transform(x)
+    
+    x_train, x_test , y_train, y_test = train_test_split(x, y, random_state=1)
+    
+    lr = LogisticRegression(penalty='l2')
+    lr.fit(x_train, y_train.ravel())
+    y_predict = lr.predict(x_test)
+    print(lr.score(x_test, y_test))
+    show_accuracy(y_predict, y_test)
+    
+    y_train[y_train==3] = 0
+    y_test[y_test == 3] = 0
+    data_train = xgb.DMatrix(x_train, label=y_train)
+    data_test = xgb.DMatrix(x_test, label=y_test)
+    watch_list = [(data_test, 'eval'), (data_train, 'train')]
+    param = {'max_depth':3, 'eta':1, 'silent':1, 'objective':'multi:softmax', 'num_class':3}
+    bst = xgb.train(param, data_train, num_boost_round=4, evals=watch_list)
+    y_predict = bst.predict(data_test)
+    show_accuracy(y_predict, y_test)
+    
